937ade59a1e4a9778f96f0c41ca50f02d6c08e00
Daniel Giathi <blacksharkchi@proton.me>
2025-03-06 19:44:31 +0300

    validate props

--- src/components/SortableWidget.jsx
+++ src/components/SortableWidget.jsx
@@ -1,19 +1,18 @@
 import { useSortable } from '@dnd-kit/sortable';
-import DashboardCard from './DashboardCard'
+import DashboardCard from './DashboardCard';
 import { CSS } from '@dnd-kit/utilities';
-
-
+import PropTypes from 'prop-types'; // Import PropTypes
 
 export const SortableWidget = (props) => {
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
-    id: props.id
-});
+    id: props.id,
+  });
 
-const style = {
-  transform: CSS.Transform.toString(transform),
-  transition,
-  touchAction: 'none'
-};
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    touchAction: 'none',
+  };
 
   return (
     <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
@@ -21,3 +20,8 @@ const style = {
     </div>
   );
 };
+
+SortableWidget.propTypes = {
+  id: PropTypes.string.isRequired, 
+};
+
